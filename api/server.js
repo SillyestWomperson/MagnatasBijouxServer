ffb2adb76902d1d6bfa3e741d21861ad07f592a3
Womperson <luis_tanese@estudante.sesisenai.org.br>
2025-08-05 07:45:46 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -12,13 +12,13 @@ const app = express();
 
 const allowedOrigins = [
 	"http://localhost:3000",
-	"https://magnatas-bijoux-server.vercel.app/",
-	"https://magnatas-bijoux.netlify.app/",
+	"https://magnatas-bijoux-server.vercel.app",
+	"https://magnatas-bijoux.netlify.app",
 ];
 
 app.use(
 	cors({
-		origin: "*",
+		origin: allowedOrigins,
 		methods: ["GET", "POST", "DELETE", "PATCH", "PUT", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
 	})
@@ -48,5 +48,6 @@ app.get("/", (req, res) => {
 }); */
 
 module.exports = app;
+
 
 
